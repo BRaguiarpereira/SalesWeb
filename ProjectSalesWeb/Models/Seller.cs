8b509af5c4bd7107672388190cdd95ee59fd0f23
Brian aguiar <briantbini@outlook.com>
2021-05-07 15:23:05 -0300

    Formatação de number and data and location na class Seller

--- ProjectSalesWeb/Models/Seller.cs
+++ ProjectSalesWeb/Models/Seller.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 namespace ProjectSalesWeb.Models
 {
     public class Seller
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString =  "{0:F2}")]
         public double BaseSalary { get; set; }
 
         // Imprementando a associação de Seller com Departament
