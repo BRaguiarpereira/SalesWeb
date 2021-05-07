71bc05b4e6646bbd8a4fc22d8b75c81a6a404f6d
Brian aguiar <briantbini@outlook.com>
2021-05-07 13:54:38 -0300

    Criando o Details e vinculando-o ao load Department

--- ProjectSalesWeb/Services/SellerService.cs
+++ ProjectSalesWeb/Services/SellerService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProjectSalesWeb.Models;
-
+using Microsoft.EntityFrameworkCore;
 namespace ProjectSalesWeb.Services
 {
     public class SellerService
@@ -24,7 +24,7 @@ namespace ProjectSalesWeb.Services
         }
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
