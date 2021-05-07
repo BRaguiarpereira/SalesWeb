7be0587db9e260bcc77fc3b6cb9f0081cd41a7b2
Brian aguiar <briantbini@outlook.com>
2021-05-07 14:31:38 -0300

    Criando Update do seller e customizando o Service Exception

--- ProjectSalesWeb/Controllers/SellersController.cs
+++ ProjectSalesWeb/Controllers/SellersController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ProjectSalesWeb.Models.ViewModels;
+using ProjectSalesWeb.Services.Exceptions;
+
 namespace ProjectSalesWeb.Controllers
 {
     public class SellersController : Controller
@@ -69,6 +71,43 @@ namespace ProjectSalesWeb.Controllers
                 NotFound();
             }
             return View(obj);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj =_sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id,Seller seller)
+        {
+            if(id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- ProjectSalesWeb/Services/SellerService.cs
+++ ProjectSalesWeb/Services/SellerService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using ProjectSalesWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using ProjectSalesWeb.Services.Exceptions;
+
 namespace ProjectSalesWeb.Services
 {
     public class SellerService
@@ -31,6 +33,22 @@ namespace ProjectSalesWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id não encontrado");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
