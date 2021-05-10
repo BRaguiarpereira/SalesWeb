4bb7fc5962e5fdf244b71fb77ed9552e1077d3f1
Brian aguiar <briantbini@outlook.com>
2021-05-10 11:15:08 -0300

    Preparando a navegação das paginas de busca

---
+++ ProjectSalesWeb/Controllers/SalesRecordController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSalesWeb.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimplesSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
