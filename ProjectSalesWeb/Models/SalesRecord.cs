61a6ef06665e9f82bc3f01e4b05b57b137512fad
Brian aguiar <briantbini@outlook.com>
2021-05-11 08:54:21 -0300

    ProjectSalesWeb com RECORDS

--- ProjectSalesWeb/Models/SalesRecord.cs
+++ ProjectSalesWeb/Models/SalesRecord.cs
@@ -1,23 +1,23 @@
 ﻿using ProjectSalesWeb.Models.Enums;
 using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectSalesWeb.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
-
         public SaleStatus Status { get; set; }
-        // Imprementando a associação de SalesRecord com o Seller 1:N
         public Seller Seller { get; set; }
 
         public SalesRecord()
         {
-
         }
 
         public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
